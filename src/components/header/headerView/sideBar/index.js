a95d0f65f2b5bf08e24c5fd610677cab084b92fe
libw <3180234769@qq.com>
2018-06-08 11:30:28 +0800

    路由

--- src/components/header/headerView/sideBar/index.js
+++ src/components/header/headerView/sideBar/index.js
@@ -39,48 +39,48 @@ class SideBar extends Component {
                         })()}>产品交易
                         </div>
                     </Link>
-                    <Link to="/DealTerrace">
-                        <div className={(() => {
-                            return this.checkedForPath('/DealTerrace')
-                        })()}>交易平台
-                        </div>
-                    </Link>
-                    <Link to="/aboutUs">
-                        <div className={(() => {
-                            return this.checkedForPath('/aboutUs')
-                        })()}>关于海豚汇
-                        </div>
-                    </Link>
-                    <Link to="/DolphinSchool">
-                        <div className={(() => {
-                            return this.checkedForPath('/DolphinSchool')
-                        })()}>合伙人计划
-                        </div>
-                    </Link>
-                    <Link to="/DolphinSchool">
-                        <div className={(() => {
-                            return this.checkedForPath('/DolphinSchool')
-                        })()}>海豚学院
-                        </div>
-                    </Link>
-                    <Link onClick={this.onClickHandle.bind(this)} to="/userCenter">
-                        <div className={(() => {
-                            return this.checkedForPath('/userCenter')
-                        })()}>个人中心
-                        </div>
-                    </Link>
+                    {/*<Link to="/DealTerrace">*/}
+                        {/*<div className={(() => {*/}
+                            {/*return this.checkedForPath('/DealTerrace')*/}
+                        {/*})()}>交易平台*/}
+                        {/*</div>*/}
+                    {/*</Link>*/}
+                    {/*<Link to="/aboutUs">*/}
+                        {/*<div className={(() => {*/}
+                            {/*return this.checkedForPath('/aboutUs')*/}
+                        {/*})()}>关于海豚汇*/}
+                        {/*</div>*/}
+                    {/*</Link>*/}
+                    {/*<Link to="/DolphinSchool">*/}
+                        {/*<div className={(() => {*/}
+                            {/*return this.checkedForPath('/DolphinSchool')*/}
+                        {/*})()}>合伙人计划*/}
+                        {/*</div>*/}
+                    {/*</Link>*/}
+                    {/*<Link to="/DolphinSchool">*/}
+                        {/*<div className={(() => {*/}
+                            {/*return this.checkedForPath('/DolphinSchool')*/}
+                        {/*})()}>海豚学院*/}
+                        {/*</div>*/}
+                    {/*</Link>*/}
+                    {/*<Link onClick={this.onClickHandle.bind(this)} to="/userCenter">*/}
+                        {/*<div className={(() => {*/}
+                            {/*return this.checkedForPath('/userCenter')*/}
+                        {/*})()}>个人中心*/}
+                        {/*</div>*/}
+                    {/*</Link>*/}
                     {/*<Link to="/ib">*/}
                         {/*<div className={(() => {*/}
                             {/*return this.checkedForPath('/ib')*/}
                         {/*})()}>代理系统*/}
                         {/*</div>*/}
                     {/*</Link>*/}
-                    <Link to="/partnerEntry">
-                        <div className={(() => {
-                            return this.checkedForPath('/partnerEntry')
-                        })()}>合伙人入口
-                        </div>
-                    </Link>
+                    {/*<Link to="/partnerEntry">*/}
+                        {/*<div className={(() => {*/}
+                            {/*return this.checkedForPath('/partnerEntry')*/}
+                        {/*})()}>合伙人入口*/}
+                        {/*</div>*/}
+                    {/*</Link>*/}
                     {/*<Link to="/partnerReg">*/}
                         {/*<div className={(() => {*/}
                             {/*return this.checkedForPath('/partnerReg')*/}
