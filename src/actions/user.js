9683e6575ec4f8f7bd189668d2b35547c4da23b3
libw <3180234769@qq.com>
2018-06-12 10:53:41 +0800

    同步

--- src/actions/user.js
+++ src/actions/user.js
@@ -47,11 +47,36 @@ export function logout(data, callback) {
     }
 }
 
+export function forgetPwd(data, callback) {
+    return dispatch => {
+
+        axios.post(config.api_url+'reg/findpassword', {
+            ...data
+        })
+            .then(function (response) {
+                if (response.data.code === 0) {
+                    // dispatch({type: 'MODIFYPWD'})
+                    callback()
+                    // localStorage.userName = response.data.data.phone
+                    // localStorage.token = response.data.data.token
+                    // localStorage.id = response.data.data.id
+                } else {
+                    // callback(response.data.msg)
+                }
+            })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+}
+
 export function modifyPwd(data, callback) {
     return dispatch => {
-        axios.patch('http://47.91.236.245:4030/user/customer/password', {
-            old_password: data.initPwd,
-            new_password: data.pwd
+        debugger
+        axios.post(config.api_url+'user/updatepassword  ', {
+            oldPassword: data.initPwd,
+            newPassword1: data.pwd,
+            newPassword2: data.confirmPwd
         })
             .then(function (response) {
                 if (response.data.code === 0) {
@@ -100,7 +125,7 @@ export function register(data, callback) {
 
 export function getBaseUserMsg(data, callback) {
     return dispatch => {
-        axios.post('http://47.91.236.245:4030/user/customer/trade-info', {})
+        axios.get('http://47.91.236.245:4030/user/customer/trade-info', {})
             .then(function (response) {
                 if (response.data.code === 0) {
                     dispatch({type: 'GET_BASEUSERMSG', data: response.data.data[0]})
@@ -120,7 +145,7 @@ export function getBaseUserMsg(data, callback) {
 
 export function getDetailMsg(data, callback) {
     return dispatch => {
-        axios.post('http://47.91.236.245:4030/user/customer/bank-card', {})
+        axios.get('http://47.91.236.245:4030/user/customer/bank-card', {})
             .then(function (response) {
                 if (response.data.code === 0) {
                     dispatch({type: 'GET_DETAILMSG', data: response.data.data})
