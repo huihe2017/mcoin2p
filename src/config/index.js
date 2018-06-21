7db07321552bd06c1a4fa86a698d3704c241a0df
Your Name <you@example.com>
2018-06-21 18:01:07 +0800

    细节修改
    细节修改

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 var config = {
 
-    // api_url: 'http://fund.coin2p.com/api/',
-    // noauth_url: 'http://fund.coin2p.com/noauth/',
+    api_url: 'http://fund.coin2p.com/api/',
+    noauth_url: 'http://fund.coin2p.com/noauth/',
 
-    api_url: 'https://www.easy-mock.com/mock/5b174f2bcbe2f85929997e75/api/',
-    noauth_url: 'https://www.easy-mock.com/mock/5b174f2bcbe2f85929997e75/api/',
+    // api_url: 'https://www.easy-mock.com/mock/5b174f2bcbe2f85929997e75/api/',
+    // noauth_url: 'https://www.easy-mock.com/mock/5b174f2bcbe2f85929997e75/api/',
 
 }
 module.exports = config
--- src/reducers/indexPage.js
+++ src/reducers/indexPage.js
@@ -4,9 +4,9 @@ export default function indexPage(state = initialState, action = {}) {
 
     switch (action.type) {
         case 'GET_INDEX_DATA':
-            state.notices = action.data.notices
-            state.funds = action.data.funds
-            state.banners = action.data.banners
+            state.notices = action.data.data.notices
+            state.funds = action.data.data.funds
+            state.banners = action.data.data.banners
             return Object.assign({}, state, {})
         default:
             return state
