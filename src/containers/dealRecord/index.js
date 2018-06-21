a727aba4d66c124594f075a3d4e0c8ca7db86315
libw <3180234769@qq.com>
2018-06-21 17:33:23 +0800

    部分页面下拉刷新

--- src/containers/dealRecord/index.js
+++ src/containers/dealRecord/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import style from "./index.css"
 import {connect} from 'react-redux'
-import {List, InputItem, Toast,Tabs,Tag} from 'antd-mobile';
+import {List, RefreshControl, Toast,Tabs,Tag} from 'antd-mobile';
 import Header from '../../components/header'
 import Footer from '../../components/footer'
 import {bindActionCreators} from 'redux'
 import {hashHistory,Link} from 'react-router'
 import {logout,getBaseUserMsg} from '../../actions/user'
 import { StickyContainer,Sticky } from 'react-sticky';
+import {ListView} from "antd-mobile/lib/index";
+import ReactDOM from "react-dom";
 
 const data = [
     {
@@ -36,24 +38,21 @@ const data = [
 class BaseUserMsg extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        const dataSource = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+        });
+        this.state = {
+            dataSource,
+            refreshing: true,
+            height: document.documentElement.clientHeight,
+        }
     }
 
-    logout() {
-        Toast.loading('正在退出', 0)
-        this.props.logout({
+    componentDidMount() {
+        // if(!this.props.user.token){
+        //     return false
+        // }
 
-        }, (errorText) => {
-            Toast.hide()
-            if (errorText) {
-                Toast.fail(errorText, 3, null, false)
-            } else {
-                hashHistory.push('/')
-            }
-        })
-    }
-
-    componentDidMount(){
         // this.props.getBaseUserMsg({
         //
         // }, (errorText) => {
@@ -64,57 +63,108 @@ class BaseUserMsg extends React.Component {
         //         //hashHistory.push('/')
         //     }
         // })
+        // Set the appropriate height
+        if(!this.lv){
+            return false
+        }
+        setTimeout(() => this.setState({
+            height: this.state.height - ReactDOM.findDOMNode(this.lv).offsetTop,
+        }), 0);
+
+        // handle https://github.com/ant-design/ant-design-mobile/issues/1588
+
+        this.lv.getInnerViewNode().addEventListener('touchstart', this.ts = (e) => {
+            this.tsPageY = e.touches[0].pageY;
+        });
+        // In chrome61 `document.body.scrollTop` is invalid
+        const scrollNode = document.scrollingElement ? document.scrollingElement : document.body;
+        this.lv.getInnerViewNode().addEventListener('touchmove', this.tm = (e) => {
+            this.tmPageY = e.touches[0].pageY;
+            if (this.tmPageY > this.tsPageY && this.scrollerTop <= 0 && scrollNode.scrollTop > 0) {
+                console.log('start pull to refresh');
+                this.domScroller.options.preventDefaultOnTouchMove = false;
+            } else {
+                this.domScroller.options.preventDefaultOnTouchMove = undefined;
+            }
+        });
     }
 
-    show(){
-        if(data.length==0){
-            return(
-                <div>
-                    <img className={style.showImg} src={require('../addressList/images/zero.png')} alt=""/>
-                    <span className={style.showTip}>
-                        暂无数据
-                    </span>
-                </div>
-            )
-        }else {
-            return(
-                <div className={style.itemBox}>
-                    {data.map(i => (
+    componentWillUnmount() {
+        this.lv.getInnerViewNode().removeEventListener('touchstart', this.ts);
+        this.lv.getInnerViewNode().removeEventListener('touchmove', this.tm);
+    }
 
-                        <div className={style.item} key={i.num} >
-                            <div className={style.itemLeft}>
-                                <div className={style.itemLeftH}>
-                                    <span className={style.itemLeftTime}>
-                                        {i.time}
-                                    </span>
-                                    <span style={ i.do=='自动赎回'?{color:'#F49193'}:{color:'#5262ff'}}>
-                                        {i.do}
-                                    </span>
-                                </div>
-                                <div className={style.itemLeftB}>
-                                    <span>
-                                        {i.name}
-                                    </span>
-                                </div>
-                            </div>
-                            <div className={style.itemRight}>
-                                <span className={style.itemRightT}>
-                                    币额
-                                </span>
-                                <span className={style.itemRightC}>
-                                    {i.coin}
-                                </span>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            )
+    onScroll = (e) => {
+        this.scrollerTop = e.scroller.getValues().top;
+        this.domScroller = e;
+    };
+
+    onRefresh = () => {
+        console.log('onRefresh');
+        if (!this.manuallyRefresh) {
+            this.setState({ refreshing: true });
+        } else {
+            this.manuallyRefresh = false;
         }
 
+        // simulate initial Ajax
+        setTimeout(() => {
+            this.rData = this.genData();
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(this.rData),
+                refreshing: false,
+                showFinishTxt: true,
+            });
+            if (this.domScroller) {
+                this.domScroller.scroller.options.animationDuration = 500;
+            }
+        }, 600);
+    };
 
+    onEndReached = (event) => {
+        // load new data
+        // hasMore: from backend data, indicates whether it is the last page, here is false
+        if (this.state.isLoading && !this.state.hasMore) {
+            return;
+        }
+        console.log('reach end', event);
+        this.setState({ isLoading: true });
+        setTimeout(() => {
+            this.rData = [...this.rData,];
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(this.rData),
+                isLoading: false,
+            });
+        }, 1000);
+    };
+
+    scrollingComplete = () => {
+        // In general, this.scrollerTop should be 0 at the end, but it may be -0.000051 in chrome61.
+        if (this.scrollerTop >= -1) {
+            this.setState({ showFinishTxt: false });
+        }
     }
 
+    renderCustomIcon() {
+        return [
+            <div key="0" className="am-refresh-control-pull">
+                <span>{this.state.showFinishTxt ? '刷新完毕' : '下拉可以刷新'}</span>
+            </div>,
+            <div key="1" className="am-refresh-control-release">
+                <span>松开立即刷新</span>
+            </div>,
+        ];
+    }
 
+    genData(pIndex = 0) {
+        const NUM_ROWS = data.length;
+        const dataArr = [];
+        for (let i = 0; i < NUM_ROWS; i++) {
+            dataArr.push(`row - ${(pIndex * NUM_ROWS) + i}`);
+        }
+        console.log(dataArr);
+        return dataArr;
+    }
 
     render() {
         function  renderTabBar(props) {
@@ -126,10 +176,56 @@ class BaseUserMsg extends React.Component {
             { title: '全部' },
             { title: '进行中' },
         ];
+
+        let pageIndex = 0;
+
+        const separator = (sectionID, rowID) => (
+            <div
+                key={`${sectionID}-${rowID}`}
+                style={{
+                    backgroundColor: '#f5f4f7',
+                    height: 10,
+                }}
+            />
+        );
+        let index = data.length - 1;
+        const row = (rowData, sectionID, rowID) => {
+            if (index < 0) {
+                index = data.length - 1;
+            }
+            const obj = data[index--];
+            return (
+                <div key={rowID} style={{ padding: '0 15px' }}>
+                    <div className={style.item} key={obj.num} >
+                        <div className={style.itemLeft}>
+                            <div className={style.itemLeftH}>
+                                <span className={style.itemLeftTime}>
+                                    {obj.time}
+                                </span>
+                                <span style={ obj.do=='自动赎回'?{color:'#F49193'}:{color:'#5262ff'}}>
+                                    {obj.do}
+                                </span>
+                            </div>
+                            <div className={style.itemLeftB}>
+                                <span>
+                                    {obj.name}
+                                </span>
+                            </div>
+                        </div>
+                        <div className={style.itemRight}>
+                            <span className={style.itemRightT}>
+                                币额
+                            </span>
+                            <span className={style.itemRightC}>
+                                {obj.coin}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            );
+        };
         return (
             <div className={style.wrap}>
-                {/*<Header/>*/}
-
                 <StickyContainer>
                     <Tabs tabs={tabs} initalPage={'t2'} renderTabBar={renderTabBar}
                     >
@@ -142,7 +238,37 @@ class BaseUserMsg extends React.Component {
                                 <Tag data-seed="logId">其他</Tag>
                             </div>
                             <div >
-                                {this.show()}
+                                {data.length==0?<div>
+                                    <img className={style.showImg} src={require('../outAddressList/images/zero.png')} alt=""/>
+                                    <span className={style.showTip}>
+                        暂无数据
+                    </span>
+                                </div>:<ListView
+                                    ref={el => this.lv = el}
+                                    dataSource={this.state.dataSource}
+
+                                    renderFooter={() => (<div style={{ padding: '0.3rem', textAlign: 'center' }}>
+                                        {this.state.isLoading ? '加载中...' : ''}
+                                    </div>)}
+                                    renderRow={row}
+                                    renderSeparator={separator}
+                                    initialListSize={5}
+                                    pageSize={5}
+                                    style={{
+                                        height: this.state.height,
+                                    }}
+                                    scrollerOptions={{ scrollbars: true, scrollingComplete: this.scrollingComplete }}
+                                    refreshControl={<RefreshControl
+                                        refreshing={this.state.refreshing}
+                                        onRefresh={this.onRefresh}
+                                        icon={this.renderCustomIcon()}
+                                    />}
+                                    onScroll={this.onScroll}
+                                    scrollRenderAheadDistance={200}
+                                    scrollEventThrottle={20}
+                                    onEndReached={this.onEndReached}
+                                    onEndReachedThreshold={10}
+                                />}
                             </div>
                         </div>
 
