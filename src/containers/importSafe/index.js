9147f8c947cc9ddf30e394f2843b14f8dc48523e
libw <3180234769@qq.com>
2018-06-29 17:14:27 +0800

    新界面

---
+++ src/containers/importSafe/index.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import style from "./index.css"
+import {connect} from 'react-redux'
+import {List, InputItem, Toast, NavBar, Icon} from 'antd-mobile';
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+import {bindActionCreators} from 'redux'
+import {hashHistory, Link} from 'react-router'
+import {logout} from '../../actions/user'
+import {setSaveCode} from '../../actions/wallet'
+class BaseUserMsg extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            submit:false
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    check(){
+        if((/^\d{6}$/.test(this.state.saveCode))&&(this.state.saveCodeConfirm==this.state.saveCode)){
+            this.setState({
+                submit:true
+            })
+        }
+        console.log(2);
+    }
+
+    render() {
+        const seasons = [
+            [
+                {
+                    label: '2013',
+                    value: '2013',
+                },
+                {
+                    label: '2014',
+                    value: '2014',
+                },
+            ],
+
+        ];
+        return (
+            <div className={style.wrap}>
+                <NavBar
+                    mode="light"
+                    icon={<Icon type="left"/>}
+                    onLeftClick={() => hashHistory.push('/baseUserMsg')
+                    }
+                    rightContent={[
+                        <div onClick={()=>{
+                            if(this.state.submit){
+                                this.props.setSaveCode({
+                                    safeCode:this.state.saveCode
+                                }, () => {
+                                    hashHistory.push('/walletIndex')
+                                })
+                            }else {
+                                Toast.fail('请确认安全码', 3, null, false)
+                            }
+
+                        }} >完成</div>,
+                    ]}
+                ></NavBar>
+                <div>
+                    <ul className={style.itemUl}>
+                        <span className={style.title}>
+                            安全码设置
+                        </span>
+                        <li className={style.itemBox}>
+
+                            <InputItem onChange={(value) => {
+                                this.setState({saveCode: value},()=>{
+                                    this.check()
+                                })
+                            }} placeholder="安全码设置（6位数字）" type="text"></InputItem>
+                        </li>
+
+                    </ul>
+
+                </div>
+                {/*<Footer/>*/}
+            </div>
+        )
+
+    }
+
+
+}
+
+function mapStateToProps(state, props) {
+    return {
+        user: state.user
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        setSaveCode: bindActionCreators(setSaveCode, dispatch)
+    }
+}
+
+BaseUserMsg = connect(mapStateToProps, mapDispatchToProps)(BaseUserMsg)
+
+
+export default BaseUserMsg;
