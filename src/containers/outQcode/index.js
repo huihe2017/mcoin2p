d25b903245a1f50819ddea82982a61d9ef2127fc
libw <3180234769@qq.com>
2018-07-03 09:42:14 +0800

    输入框

--- src/containers/outQcode/index.js
+++ src/containers/outQcode/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import style from "./index.css"
 import {connect} from 'react-redux'
-import { List,InputItem,Button,NavBar,Icon} from 'antd-mobile';
+import { List,InputItem,Button,NavBar,Icon,Modal} from 'antd-mobile';
 import Header from '../../components/header'
 import {hashHistory} from "react-router";
+
+const prompt = Modal.prompt;
 
 class AboutUs extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            sum:''
         }
     }
 
@@ -26,8 +28,11 @@ class AboutUs extends React.Component {
                     <div className={style.qcodeBox}>
                         <div className={style.header}>
 
-                            <a className={style.nameA} href="">
-                                <span className={style.name}>设置金额</span>
+                            <a className={style.nameA} href="javascript:void (0)" onClick={()=> prompt('金额', '', [
+                                { text: '取消' },
+                                { text: '确定', onPress: value => this.setState({sum:value},()=>console.log(`输入的内容:${value}`)) },
+                            ])}>
+                                <span className={style.name}>{this.state.sum==''?'设置金额':this.state.sum}</span>
                                 <img className={style.nameImg} src={require('./images/edit.png')} alt=""/>
                             </a>
                         </div>
@@ -51,6 +56,7 @@ class AboutUs extends React.Component {
                         保存到相册
                     </div>
                 </div>
+
             </div>
         )
 
