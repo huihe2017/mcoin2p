0b7e78d502a0a8718ed53151209f28833466e196
libw <3180234769@qq.com>
2018-08-03 14:08:18 +0800

    1

--- src/containers/recordDetail/index.js
+++ src/containers/recordDetail/index.js
@@ -60,7 +60,7 @@ class BaseUserMsg extends React.Component {
                                     {this.getOrderStatus(this.props.fund.tradeDatails.status)}
                                 </span>
                                 <span className={style.userTime}>
-                                    {this.props.fund.tradeDatails.currency} {this.props.fund.tradeDatails.mount}
+                                    {this.props.fund.tradeDatails.currency} {this.props.fund.tradeDatails.amount}
                                 </span>
                             </div>
                         </a>
